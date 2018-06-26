32a1c5573498f815a238506e58f68ffafe49161b
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-06-26 17:56:20 +0200

    We now add non-zero non-negative integers.

--- test/source/io/montanus/math/test/AddFractionsTest.java
+++ test/source/io/montanus/math/test/AddFractionsTest.java
@@ -3,6 +3,8 @@ package io.montanus.math.test;
 import io.montanus.math.Fraction;
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.Assert.assertEquals;
 
 public class AddFractionsTest {
@@ -22,5 +24,11 @@ public class AddFractionsTest {
     public void zeroPlusNonZero() {
         final Fraction sum = new Fraction(0).plus(new Fraction(5));
         assertEquals(5, sum.intValue());
+    }
+
+    @Test
+    public void nonZeroNonNegativeIntegers() {
+        final Fraction sum = new Fraction(3).plus(new Fraction(4));
+        assertEquals(7, sum.intValue());
     }
 }
