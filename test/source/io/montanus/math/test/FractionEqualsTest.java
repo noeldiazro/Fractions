6d48e64cea265a38e83aa61f3fc7de624af610c5
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-06-27 19:38:55 +0200

    I feel confident enough that equals() works, except for reducing.

--- test/source/io/montanus/math/test/FractionEqualsTest.java
+++ test/source/io/montanus/math/test/FractionEqualsTest.java
@@ -4,10 +4,26 @@ import io.montanus.math.Fraction;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class FractionEqualsTest {
     @Test
     public void sameNumeratorsAndSameDenominators() {
         assertEquals(new Fraction(3, 5), new Fraction(3, 5));
+    }
+
+    @Test
+    public void differentNumerators() {
+        assertNotEquals(new Fraction(1, 3), new Fraction(2, 3));
+    }
+
+    @Test
+    public void differentDenominators() {
+        assertNotEquals(new Fraction(1, 3), new Fraction(2, 3));
+    }
+
+    @Test
+    public void wholeNumberEqualsFraction() {
+        assertEquals(new Fraction(8), new Fraction(8, 1));
     }
 }
