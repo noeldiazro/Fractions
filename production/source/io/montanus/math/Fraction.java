cc6021a4dd37c33af17b446956e0beb05803ed70
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-06-27 11:58:41 +0200

    We no longer fake numerator.

--- production/source/io/montanus/math/Fraction.java
+++ production/source/io/montanus/math/Fraction.java
@@ -3,14 +3,17 @@ package io.montanus.math;
 public class Fraction {
     private final int denominator;
     private final int integerValue;
+    private final int numerator;
 
     public Fraction(int integerValue) {
         this.integerValue = integerValue;
+        this.numerator = integerValue;
         this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
         this.integerValue = numerator;
+        this.numerator = numerator;
         this.denominator = denominator;
     }
 
@@ -23,7 +26,7 @@ public class Fraction {
     }
 
     public int getNumerator() {
-        return 3;
+        return numerator;
     }
 
     public int getDenominator() {
